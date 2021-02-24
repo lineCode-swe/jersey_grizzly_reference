db8d968177f2c62368994adbfff843dc7ab17f3d
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2021-02-24 14:59:02 +0100

    Immagine Docker ora funzionante
    Cambiato URI da http://localhost:8080/myapp/ a http://0.0.0.0:8080/myapp/ in modo da ascoltare su tutti gli indirizzi ed essere compatibile con la containerizzazione

--- src/main/java/org/linecode/Main.java
+++ src/main/java/org/linecode/Main.java
@@ -13,8 +13,10 @@ import java.net.URI;
  *
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    // Base URI the Grizzly HTTP server will listen on.
+    // ATTENZIONE: url cambiato da localhost a 0.0.0.0 per funzionare all'interno
+    // di un container docker. Fonte: https://yurisubach.com/2016/07/14/jersey-dockerize/
+    public static final String BASE_URI = "http://0.0.0.0:8080/myapp/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
